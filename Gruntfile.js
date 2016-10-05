294902ccc6ea37be9b900f4fae2ffa0df187c7e0
Thomas Jakobi <thomas.jakobi@partout.info>
2016-10-05 11:03:31 +0200

    Update gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
                     srcBasePath: 'develop/toggletvset/',
                     host: '<%= sshconfig.host %>',
                     username: '<%= sshconfig.username %>',
-                    privateKey: grunt.file.read("/Users/jako/.ssh/id_dsa"),
+                    privateKey: '<%= sshconfig.privateKey %>',
                     passphrase: '<%= sshconfig.passphrase %>',
                     showProgress: true
                 }
