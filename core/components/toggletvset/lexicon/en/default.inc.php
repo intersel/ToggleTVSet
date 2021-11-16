61c1b4f15f3c946679bfcfd42805ebf1c80aba82
Thomas Jakobi <thomas.jakobi@partout.info>
2021-11-16 15:10:04 +0100

    Update lexicon

--- core/components/toggletvset/lexicon/en/default.inc.php
+++ core/components/toggletvset/lexicon/en/default.inc.php
@@ -7,7 +7,7 @@
  */
 
 $_lang['setting_toggletvset.debug'] = 'Debug';
-$_lang['setting_toggletvset.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_toggletvset.debug_desc'] = 'Log debug information in the MODX error log.';
 $_lang['setting_toggletvset.toggletvs'] = 'Toggling Template Variables';
 $_lang['setting_toggletvset.toggletvs_desc'] = 'Comma separated list of template variable ids that should toggle the visibility of other template variables.';
 $_lang['setting_toggletvset.toggletvs_clearhidden'] = 'Clear Hidden Template Variables';
